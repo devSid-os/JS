8fe7ad4500540698f368d8ee0de7bacd64270277
devSid-os <siddjain341@gmail.com>
2023-11-26 19:59:13 +0530

    Filter functionality implemented

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,19 @@
 let localStorageTasks = JSON.parse(localStorage.getItem("taskList")) || [];
+let pendingTasks = [];
+let completedTasks = [];
 const overlay = document.querySelector("#overlay");
 const loader = document.querySelector("#loader");
 const taskListDiv = document.querySelector("#taskListDiv");
+let selectedFilter = "pending";
+
+console.log(localStorageTasks)
+
+function filterTasks() {
+    pendingTasks = localStorageTasks.filter(task => task.status === "pending" && task);
+    completedTasks = localStorageTasks.filter(task => task.status !== "pending" && task);
+}
+
+filterTasks();
 
 function showEmptyMessage() {
     const heading3 = document.createElement("h3");
@@ -11,19 +23,11 @@ function showEmptyMessage() {
     taskListDiv.appendChild(heading3);
 }
 
-function deleteTask(e) {
-    const taskDivId = e.target.parentElement.parentElement.id;
-    localStorageTasks = localStorageTasks.filter(task => task.id !== taskDivId);
-    document.getElementById(taskDivId).remove();
-    localStorage.setItem("taskList", JSON.stringify(localStorageTasks));
-    if (!localStorageTasks.length) showEmptyMessage();
-}
-
 function createTaskDiv(task) {
     // create parent task div
     const taskDiv = document.createElement("div");
     taskDiv.id = task.id;
-    taskDiv.classList.add("w-full", "flex", "justify-between", "border", "border-[#fa992f]", "rounded-lg");
+    taskDiv.classList.add("w-full", "flex", "justify-between", "border", `${task.status === "pending" ? "border-[#fa992f]" : "border-[#96EFFF]"}`, "rounded-lg");
     // create task name
     const taskName = document.createElement("p");
     taskName.setAttribute("title", `Created On: ${task.date}\nStatus: ${task.status.toUpperCase()}`);
@@ -37,9 +41,10 @@ function createTaskDiv(task) {
     editButton.setAttribute("title", "Edit Task");
     editButton.classList.add("text-[#0d9a77]", "font-[500]", "text-md", "md:text-lg", "focus:outline-none", "fa", "fa-edit");
     // Complete Button
-    const completeButton = document.createElement("button");
-    completeButton.setAttribute("title", "Mark as complete");
-    completeButton.classList.add("text-[#fa9924]", "font-[500]", "text-md", "md:text-lg", "focus:outline-none", "fa", "fa-check");
+    const statusButton = document.createElement("button");
+    statusButton.addEventListener("click", (e) => markAsCompleteTask(e));
+    statusButton.setAttribute("title", `${task.status === 'pending' ? "Mark as complete" : "Mark as uncomplete"}`);
+    statusButton.classList.add("text-[#fa9924]", "font-[500]", "text-md", "md:text-lg", "focus:outline-none", "fa", `fa-${task.status === "pending" ? "check" : "exclamation-circle"}`);
     // Delete Button
     const deleteButton = document.createElement("button");
     deleteButton.addEventListener("click", (e) => deleteTask(e));
@@ -47,7 +52,7 @@ function createTaskDiv(task) {
     deleteButton.classList.add("text-[#d71437]", "font-[500]", "text-md", "md:text-lg", "focus:outline-none", "fa", "fa-trash");
 
     buttonDiv.appendChild(editButton);
-    buttonDiv.appendChild(completeButton);
+    buttonDiv.appendChild(statusButton);
     buttonDiv.appendChild(deleteButton);
 
     // append taskName to taskDiv
@@ -56,7 +61,24 @@ function createTaskDiv(task) {
     taskDiv.append(buttonDiv);
 
     taskListDiv.appendChild(taskDiv);
+}
 
+function markAsCompleteTask(e) {
+    const taskDivId = e.target.parentElement.parentElement.id;
+    localStorageTasks.map(task => task.id === taskDivId ? task.status = "completed" : task);
+    localStorage.setItem("taskList", JSON.stringify(localStorageTasks));
+    pendingTasks = pendingTasks.filter(task => task.id !== taskDivId);
+    document.getElementById(taskDivId).remove();
+    console.log(taskListDiv.children);
+    if (!taskListDiv.children.length) showEmptyMessage();
+}
+
+function deleteTask(e) {
+    const taskDivId = e.target.parentElement.parentElement.id;
+    localStorageTasks = localStorageTasks.filter(task => task.id !== taskDivId);
+    document.getElementById(taskDivId).remove();
+    localStorage.setItem("taskList", JSON.stringify(localStorageTasks));
+    if (!taskListDiv.children.length) showEmptyMessage();
 }
 
 function addTaskToLocalStorage(taskName) {
@@ -77,10 +99,8 @@ function addTaskToLocalStorage(taskName) {
     overlay.classList.add("hidden");
     loader.classList.add("hidden");
     // Hide empty message on adding task
-    const emptyMessage = document.querySelector("#emptyMessage");
-    if (emptyMessage) emptyMessage.classList.add("hidden");
-
-    createTaskDiv(object);
+    selectedFilter === "pending" && createTaskDiv(object);
+    if (taskListDiv.children.length) document.getElementById("emptyMessage").remove();
 }
 
 function addTask() {
@@ -99,11 +119,26 @@ function addTask() {
     setTimeout(() => addTaskToLocalStorage(taskName), 1000);
 }
 
-if (!localStorageTasks.length) {
+function filterTaskListDiv(e) {
+    // console.log("hello")
+    filterTasks();
+    while (taskListDiv.firstChild) taskListDiv.removeChild(taskListDiv.firstChild);
+    if (e.value === "pending") {
+        selectedFilter = "pending";
+        pendingTasks.map(task => createTaskDiv(task));
+    }
+    else {
+        selectedFilter = "completed";
+        completedTasks.map(task => createTaskDiv(task));
+    }
+
+    if (!taskListDiv.children.length) showEmptyMessage();
+
+}
+
+if (!pendingTasks.length) {
     showEmptyMessage();
 }
 else {
-    localStorageTasks.map((task, index) => {
-        createTaskDiv(task);
-    })
+    pendingTasks.map((task, index) => createTaskDiv(task));
 }
