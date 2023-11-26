dbb04d658d11cc67dfdab64534abf328999e3abe
devSid-os <siddjain341@gmail.com>
2023-11-26 17:32:09 +0530

    Add Task functionality completly added

--- dist/script.js
+++ dist/script.js
@@ -16,17 +16,48 @@ function showEmptyMessage() {
 function createTaskDiv(task) {
     // create parent task div
     const taskDiv = document.createElement("div");
-    // taskDiv.classList.add("w-full")
+    taskDiv.classList.add("w-full", "flex", "justify-between", "border", "border-[#fa992f]", "rounded-lg");
+    // create task name
+    const taskName = document.createElement("p");
+    taskName.classList.add("flex-1", "dark:text-white", "md:text-lg", "p-3", "md:p-4");
+    taskName.textContent = task.taskName;
+    // create button div
+    const buttonDiv = document.createElement("div");
+    buttonDiv.classList.add("flex", "justify-evenly", "items-center", "gap-4", "px-4");
+    // Edit Button
+    const editButton = document.createElement("button");
+    editButton.classList.add("text-[#0d9a77]", "font-[500]", "text-md", "md:text-lg", "focus:outline-none", "fa", "fa-edit");
+    // Complete Button
+    const completeButton = document.createElement("button");
+    completeButton.classList.add("text-[#fa9924]", "font-[500]", "text-md", "md:text-lg", "focus:outline-none", "fa", "fa-check");
+    // Delete Button
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("text-[#d71437]", "font-[500]", "text-md", "md:text-lg", "focus:outline-none", "fa", "fa-trash");
+
+    buttonDiv.appendChild(editButton);
+    buttonDiv.appendChild(completeButton);
+    buttonDiv.appendChild(deleteButton);
+
+
+    // append taskName to taskDiv
+    taskDiv.appendChild(taskName);
+    // append buttonDiv to taskDiv
+    taskDiv.append(buttonDiv);
+
+    taskListDiv.appendChild(taskDiv);
+
 }
 
 function addTaskToLocalStorage(taskName) {
     const d = new Date;
-    // add task in object format to array
-    localStorageTasks.push({
+    const object = {
         taskName: taskName.value,
         date: d.toLocaleDateString(),
-        id: d.toISOString()
-    });
+        id: d.toISOString(),
+        status: "pending"
+    };
+    // add task in object format to array
+    localStorageTasks.push(object);
     // save array to localstorage
     localStorage.setItem("taskList", JSON.stringify(localStorageTasks));
     // Reset input value
@@ -34,8 +65,10 @@ function addTaskToLocalStorage(taskName) {
     // hide loader
     overlay.classList.add("hidden");
     loader.classList.add("hidden");
+    // Hide empty message on adding task
     const emptyMessage = document.querySelector("#emptyMessage");
     if (emptyMessage) emptyMessage.classList.add("hidden");
+    createTaskDiv(object);
 }
 
 function addTask() {
@@ -59,7 +92,7 @@ if (!localStorageTasks.length) {
     showEmptyMessage();
 }
 else {
-    localStorageTasks.map((task,index) => {
-        console.log(index,task)
+    localStorageTasks.map((task, index) => {
+        createTaskDiv(task);
     })
 }
